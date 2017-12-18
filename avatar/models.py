b25112f7b851f7f1fcb45ca074de33b88a54b6d7
Anna Maste <skulegirlanna@gmail.com>
2017-12-18 14:39:28 -0500

    Convert RGBA to RGB since PIL now fails to convert to JPG if there's an alpha channel

--- avatar/models.py
+++ avatar/models.py
@@ -146,7 +146,7 @@ class Avatar(models.Model):
                 else:
                     diff = int((h - w) / 2)
                     image = image.crop((0, diff, w, h - diff))
-                if image.mode not in ("RGB", "RGBA"):
+                if image.mode not in ("RGB"):
                     image = image.convert("RGB")
                 image = image.resize((size, size), settings.AVATAR_RESIZE_METHOD)
                 thumb = six.BytesIO()
